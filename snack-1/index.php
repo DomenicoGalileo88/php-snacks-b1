a3c4bfff18078cb39a94d69f9b373d078f49edd3
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-05-20 12:43:06 +0200

    initial commit
    struttura dati

---
+++ snack-1/index.php
@@ -0,0 +1,38 @@
+<!-- 
+Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+ -->
+
+ <?php 
+ /* Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+ Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite */
+ $matchs = [
+    [
+        'casa' => 'Olimpia Milano',
+        'ospite' => 'Cefalù',
+        'punti casa' => '55',
+        'punti ospite' => '60',
+    ],
+    [
+        'casa' => 'Napoli',
+        'ospite' => 'Roma',
+        'punti casa' => '70',
+        'punti ospite' => '60',
+    ],
+    [
+        'casa' => 'Bari',
+        'ospite' => 'Messina',
+        'punti casa' => '65',
+        'punti ospite' => '50',
+    ],
+    [
+        'casa' => 'Modena',
+        'ospite' => 'Parma',
+        'punti casa' => '60',
+        'punti ospite' => '70',
+    ],
+];
+
+var_dump($matchs);
+ ?>
